e8c6d7f16250378c06429ada07874141f913b3bc
florent champigny <champigny.florent@gmail.com>
2018-06-21 17:05:40 +0200

    removed unused code

--- app/src/main/java/com/naman14/timber/fragments/MainFragment.java
+++ app/src/main/java/com/naman14/timber/fragments/MainFragment.java
@@ -54,22 +54,12 @@ public class MainFragment extends Fragment {
         View rootView = inflater.inflate(
                 R.layout.fragment_main, container, false);
 
-        Toolbar toolbar = (Toolbar) rootView.findViewById(R.id.toolbar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-
-        final ActionBar ab = ((AppCompatActivity) getActivity()).getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
-
 
         viewPager = (ViewPager) rootView.findViewById(R.id.viewpager);
         if (viewPager != null) {
             setupViewPager(viewPager);
             viewPager.setOffscreenPageLimit(2);
         }
-
-        TabLayout tabLayout = (TabLayout) rootView.findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
 
         return rootView;
 
